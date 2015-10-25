f4ae70e5610198f6a926eff965c7a9f130ed0f6b
blacknova404 <ciler819@gmail.com>
2015-10-25 14:43:58 -0400

    Added vigenere

--- substitution/vigenere.py
+++ substitution/vigenere.py
@@ -1,1 +1,21 @@
 __author__ = 'Chris Iler'
+
+def get_bigrams(text):
+    out = {}
+    temp = {}
+    text = text.lower()
+    text = text.replace(" ", "")
+    for i in range(len(text)-1):
+        bigram = text[i:i+2]
+        if bigram in temp:
+            temp[bigram] += 1
+        else:
+            temp[bigram] = 1
+    for key in temp:
+        if not temp[key] == 1:
+            out[key] = temp[key]
+    import operator
+    return sorted(out.items(), key=operator.itemgetter(1), reverse=True)
+
+if __name__ == '__main__':
+    print(get_bigrams("the quick brown fox jumped over the lazy dogth"))
